b56f695ebebcc9642f1fc09a80081a58aac891aa
尹岩岩 <yinyanyan8704@gmail.com>
2019-11-01 11:12:03 +0800

    使用WebSockets

--- my_flutter_app/lib/main.dart
+++ my_flutter_app/lib/main.dart
@@ -1,77 +1,84 @@
-import 'dart:async';
-import 'dart:convert';
-import 'dart:io';
-
+import 'package:flutter/foundation.dart';
+import 'package:web_socket_channel/io.dart';
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
-
-// Future<Post> fetchPost() async {
-//   final response =
-//       await http.get('https://jsonplaceholder.typicode.com/posts/1');
-//   final responseJson = json.decode(response.body);
-
-//   return new Post.fromJson(responseJson);
-// }
-
-Future<Post> fetchPost() async {
-  final response = await http.get(
-    'https://jsonplaceholder.typicode.com/posts/1',
-    headers: {HttpHeaders.AUTHORIZATION: "Basic your_api_token_here"},
-  );
-  final responseJson = json.decode(response.body); 
-  
-  return new Post.fromJson(responseJson); 
-}
-
-class Post {
-  final int userId;
-  final int id;
-  final String title;
-  final String body;
-
-  Post({this.userId, this.id, this.title, this.body});
-
-  factory Post.fromJson(Map<String, dynamic> json) {
-    return new Post(
-      userId: json['userId'],
-      id: json['id'],
-      title: json['title'],
-      body: json['body'],
-    );
-  }
-}
+import 'package:web_socket_channel/web_socket_channel.dart';
 
 void main() => runApp(new MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final title = 'WebSocket Demo';
     return new MaterialApp(
-      title: 'Fetch Data Example',
-      theme: new ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: new Scaffold(
-        appBar: new AppBar(
-          title: new Text('Fetch Data Example'),
-        ),
-        body: new Center(
-          child: new FutureBuilder<Post>(
-            future: fetchPost(),
-            builder: (context, snapshot) {
-              if (snapshot.hasData) {
-                return new Text(snapshot.data.title);
-              } else if (snapshot.hasError) {
-                return new Text("${snapshot.error}");
-              }
-
-              // By default, show a loading spinner
-              return new CircularProgressIndicator();
-            },
-          ),
-        ),
+      title: title,
+      home: new MyHomePage(
+        title: title,
+        channel: new IOWebSocketChannel.connect('ws://echo.websocket.org'),
       ),
     );
   }
 }
 
+class MyHomePage extends StatefulWidget {
+  final String title;
+  final WebSocketChannel channel;
+
+  MyHomePage({Key key, @required this.title, @required this.channel})
+      : super(key: key);
+
+  @override
+  _MyHomePageState createState() => new _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  TextEditingController _controller = new TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text(widget.title),
+      ),
+      body: new Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: new Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            new Form(
+              child: new TextFormField(
+                controller: _controller,
+                decoration: new InputDecoration(labelText: 'Send a message'),
+              ),
+            ),
+            new StreamBuilder(
+              stream: widget.channel.stream,
+              builder: (context, snapshot) {
+                return new Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 24.0),
+                  child: new Text(snapshot.hasData ? '${snapshot.data}' : ''),
+                );
+              },
+            )
+          ],
+        ),
+      ),
+      floatingActionButton: new FloatingActionButton(
+        onPressed: _sendMessage,
+        tooltip: 'Send message',
+        child: new Icon(Icons.send),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+
+  void _sendMessage() {
+    if (_controller.text.isNotEmpty) {
+      widget.channel.sink.add(_controller.text);
+    }
+  }
+
+  @override
+  void dispose() {
+    widget.channel.sink.close();
+    super.dispose();
+  }
+}
